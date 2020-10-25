e8d06ed1139f46ad1abb07ace45a4a58ffb8a360
Sanjuron <julien_nn@live.fr>
2020-10-25 09:56:35 +0100

    simple app design / assets not working

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,83 @@ class SanjuronCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
         title: Text("Sanjuron id Card"),
+        centerTitle: true,
+        backgroundColor: Colors.green[700],
+        elevation: 0.0, // pour supprimer l'effet d'ombre
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/thumb.png'),
+                radius: 35.0,
+              ),
+            ),
+            Divider(
+              height:60.0,
+              color: Colors.amberAccent[200],
+            ),
+            Text(
+              'NOM',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              'Sanjuron',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30.0),
+            Text(
+              'NIVEAU ACTUEL',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              '8',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30.0),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(height: 30.0),
+                Text(
+                    "sanjuron@exemple.com",
+                    style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                    ),
+                  )
+              ]
+            ),
+          ],
+        ),
       ),
     );
   }
